313cc89c706b09272c96f1255ada8e77e0befe0e
Aawantika Sahu <aawantika@gmail.com>
2014-06-21 10:42:54 -0700

    last commit

--- VuforiaSamples-3-0-5_0/src/com/qualcomm/vuforia/samples/VuforiaSamples/LanguageActivity.java
+++ VuforiaSamples-3-0-5_0/src/com/qualcomm/vuforia/samples/VuforiaSamples/LanguageActivity.java
@@ -1,9 +1,12 @@
 package com.qualcomm.vuforia.samples.VuforiaSamples;
 
+import java.util.Locale;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,6 +17,7 @@ public class LanguageActivity extends Activity {
 	private String translated;
 	private String word;
 	private String language;
+	private TextToSpeech ttobj;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -21,20 +25,56 @@ public class LanguageActivity extends Activity {
 		setContentView(R.layout.activity_language);
 
 		word = getIntent().getStringExtra("word");
-		language = getIntent().getStringExtra("language");
 		word = fixWord(word);
-		
+		language = getIntent().getStringExtra("language");
+
 		TextView english = (TextView) findViewById(R.id.english);
 		english.setText(word);
 		TextView anotherLanguageLabel = (TextView) findViewById(R.id.languageLabel);
 		System.out.println("here1: " + calcLanguage(language));
 		anotherLanguageLabel.setText(calcLanguage(language));
-		
+
 		new TranslateTask().execute("");
+
+		ttobj = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+			@Override
+			public void onInit(int status) {
+				if (status != TextToSpeech.ERROR) {
+					ttobj.setLanguage(fixLanguage(language));
+				}
+			}
+		});
+	}
+	
+	public void onSoundClick(View view) {
+		ttobj.speak(fixWord(word), TextToSpeech.QUEUE_FLUSH, null);
+	}
+
+	private Locale fixLanguage(String input) {
+		System.out.println("here1 language: "+ input);
+		if (input.equals("en")) {
+			return Locale.ENGLISH;
+		} else if (input.equals("ru")) {
+			return Locale.ENGLISH;
+		} else if (input.equals("uk")) {
+			return Locale.ENGLISH;
+		} else if (input.equals("de")) {
+			return Locale.GERMAN;
+		} else if (input.equals("fr")) {
+			return Locale.FRENCH;
+		} else if (input.equals("es")) {
+			return Locale.ENGLISH;
+		} else if (input.equals("it")) {
+			return Locale.ITALIAN;
+		} else if (input.equals("tr")) {
+			return Locale.ENGLISH;
+		}
+		
+		return Locale.ENGLISH;
 	}
 
 	private String fixWord(String input) {
-		input.replaceAll("\\d+.*", "");
+		input.replaceAll("[0-9]","");
 		return input;
 	}
 
@@ -80,7 +120,7 @@ public class LanguageActivity extends Activity {
 
 		@Override
 		protected void onPostExecute(String result) {
-			
+
 			TextView anotherLanguage = (TextView) findViewById(R.id.language);
 			anotherLanguage.setText(translated);
 		}
